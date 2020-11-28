c55b6908f0a239042ef92f95680bde866543a172
Bo Kane <bokane@Bo.K-MBPro>
2020-11-28 11:12:04 -0500

    revised some ish

--- _site/upload.sh
+++ _site/upload.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+set -x
 
-msg = "Site updated at $(date "+%Y-%m-%d")"
+echo -e "\e[92mUpdating site..."
 
-git commit -am $msg
+git add .
+
+d=$(date "+%Y-%m-%d")
+tmp_dir=$(mktemp -d ../tmp)
+
+
+git commit -am "Site updated at $d"
+
+echo -e "\e[92mPushing current directory to master"
 
 git push origin master
+
+echo -e "\e[92mMoving built site to tmp"
+
+mv _site/* 
+
--- upload.sh
+++ upload.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 set -x
 
+echo -e "\e[92mUpdating site..."
+
 git add .
 
 d=$(date "+%Y-%m-%d")
+tmp_dir=$(mktemp -d ../tmp)
 
-echo $d
 
 git commit -am "Site updated at $d"
 
+echo -e "\e[92mPushing current directory to master"
+
 git push origin master
+
+echo -e "\e[92mMoving built site to tmp"
+
+mv _site/* 
+
